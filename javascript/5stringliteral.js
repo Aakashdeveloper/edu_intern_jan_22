6c07fa58a8c32a166b1a3a5fda638ca6628616b9
aakashdeveloper <ahanda205@gmail.com>
2022-02-27 15:48:43 +0530

    darklight

--- javascript/5stringliteral.js
+++ javascript/5stringliteral.js
@@ -6,9 +6,24 @@ let rating = 3.8
 //es5
 let a ="An "+name+" is an "+type+" movie with rating of "+rating+" and from ind "+ind+"."
 //'An Avengers is an Actions movie with rating of 3.8 and from ind Hollywood.'
-console.log("a>>>",a)
+console.log(">>>a",a)
 
 //es6
 let b = `An ${name} is an ${type} movie with rating of ${rating} and from ind ${ind}.`
 //'An Avengers is an Actions movie with rating of 3.8 and from ind Hollywood.'
 console.log(">>>b",b)
+
+
+
+function checkUser(userinput){
+    if(userinput == "John"){
+        console.log(`Hi ${userinput} you are admin`)
+    }else if(userinput == "Bhumika"){
+        console.log(`Hi ${userinput} you are super admin`)
+    }else{
+        console.log(`Hi ${userinput} you unknown`)
+    }
+}
+
+checkUser("John")
+=
